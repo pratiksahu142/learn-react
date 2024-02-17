66c153d8df019ea4ab055c05c4338fecbbc588df
pratiksahu <pratik.sahu1421996@gmail.com>
2024-02-17 15:20:14 -0500

    React Activity qcomps 15 Feb

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,21 @@ import styles from '@/styles/Home.module.css'
 //import PackingList from './components/props_item'
 //import List from './components/list_plain'
 //import List from './components/list_keys'
+//import Gallery from './components/whyState'
+//import PersonData from './components/stateObj'
 
 import Profile from './qcomps/profile_mistake'
 import MyComp from './qcomps/firstcomp'
 import Bio from './qcomps/bios'
 import TodoList from './qcomps/todos'
-import Gallery from './qcomps/gallery_props'
+//import Gallery from './qcomps/gallery_props'
 import PackingList from './qcomps/props_item'
 import List from './qcomps/list_keys_id'
 import RecipeList from './qcomps/recipes'
+import Gallery from './qcomps/state'
+import Form from './qcomps/stuckForm'
+import FeedbackForm from './qcomps/thankYouCrash'
+import RequestTracker from './qcomps/shoppingCart'
 
 export default function Home() {
   return (
@@ -29,6 +35,10 @@ export default function Home() {
         <PackingList />
         <List />
         <RecipeList />
+        <Gallery />
+        <Form />
+        <FeedbackForm />
+        <RequestTracker />
     </div>
   )
 }
